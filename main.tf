786f0eec7b260baf151508feae73c53b58fe0ba6
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-07-07 09:46:54 +0200

    gcp test

--- main.tf
+++ main.tf
@@ -1,28 +1,47 @@
-provider "aws" {
-  version = "2.33.0"
-
-  region = var.aws_region
+provider "google-beta" {
+  credentials = "${file("account.json")}"
+  project     = "tilehuria"
+  region      = "us-central1"
+  zone        = "us-central1-c"
 }
 
-provider "random" {
-  version = "2.2"
-}
+resource "google_compute_instance" "vm_instance" {
+  name         = "rabbitmq"
+  machine_type = "f1-micro"
 
-resource "random_pet" "table_name" {}
-
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
   }
 
-  tags = {
-    user_name = var.tag_user_name
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config = {}
   }
 }
+
+# resource "random_pet" "table_name" {}
+
+
+# resource "aws_dynamodb_table" "tfc_example_table" {
+#   name = "${var.db_table_name}-${random_pet.table_name.id}"
+
+
+#   read_capacity  = var.db_read_capacity
+#   write_capacity = var.db_write_capacity
+#   hash_key       = "UUID"
+
+
+#   attribute {
+#     name = "UUID"
+#     type = "S"
+#   }
+
+
+#   tags = {
+#     user_name = var.tag_user_name
+#   }
+# }
+
